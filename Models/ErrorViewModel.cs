6d64da3825a7abbb4a19f387564636057b020adb
faaa1 <faaa1@DESKTOP-LS117MP>
2024-10-14 14:39:42 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MidTermReview.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
